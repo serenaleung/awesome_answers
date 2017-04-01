afb4e9c7fe0b5200829f35ffd160aa295d6b3a3c
Serena Leung <serenaleung1@gmail.com>
2017-03-31 22:59:37 -0700

    Refactoring using one form to populate in multiple places and simplified code. Added flash messages and styled the pages.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,11 @@
 class QuestionsController < ApplicationController
+  # the `before_action` method registers another method in this case it's the
+  # `find_quesiton` method which will be executed just before the actions you
+  # specify in the `only` array. Keep in mind that the method that gets executed
+  # as a `before_action` happens within the same request/response cycle so if
+  # you define an instance variable you can use it within the action / views
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
+
   def new
     # we instaniate a new `Question` object because it will help us in
     # generating the form in `views/new.html.erb`. We have to make it an
@@ -29,14 +36,26 @@ class QuestionsController < ApplicationController
     if @question.save
       # redirct_to question_path({id: @question.id})
       # redirect_to question_path(@question.id)
-      redirect_to question_path(@question)
+
       #
       # render :show
       # render plain: 'Question created successfully'
+
+      flash[:notice] = 'Question created!'
+      redirect_to question_path(@question)
+
     else
       # because you're in the same path already
-      render :new
       # render 'questions/new'
+
+
+      # if you want to show a flash message when you're doing `render`instead
+      # of `redirect` meaning that you want to show the flash message within the
+      # same requestion/response cycle, then you will need to use flash.now
+      flash.now[:alert] = 'Please fix errors below'
+      render :new
+
+
       # render plain: "Errors: #{question.errors.full_messages.join(', ')}"
     end
     # render json: params
@@ -45,7 +64,9 @@ class QuestionsController < ApplicationController
   def show
     # {"controller":"questions","action":"show","id":"4"}
     # if you take json out it'll render app/views/questions/show.html.erb
-    @question = Question.find params[:id]
+
+    # @question = Question.find params[:id]
+
     # render json: params
   end
 
@@ -54,19 +75,21 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find params[:id]
+    # @question = Question.find params[:id]
     # render plain: 'inside questions edit'
   end
 
   def update
-    @question = Question.find params[:id]
+    # @question = Question.find params[:id]
     # find the new question to update it
     # get the params to make sure you're updating the right post
-    question_params = params.require(:question).permit([:title, :body])
+
+    # question_params = params.require(:question).permit([:title, :body])
+
     # update it
     # if succeeds redirect to the show page
     if @question.update(question_params)
-      redirect_to question_path(@question)
+      redirect_to question_path(@question), notice: 'Question updated'
     else
     # if doesn't show the edit page
       render :edit
@@ -75,11 +98,26 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    question = Question.find params[:id]
-    question.destroy
-    redirect_to questions_path
+    # question = Question.find params[:id]
+    # question.destroy
+
+    # local variable because only redirecting
+    # only need instance variables if sharing with a template
+
+    @question.destroy
+    redirect_to questions_path, notice: 'Question deleted'
     # render plain: 'in questions destroy'
   end
 
+  def find_question
+    @question = Question.find params[:id]
+  end
+
+  def question_params
+     # the line below is what's called "Strong Parameters" feautre that was added
+     # to Rails starting with version 4 to help developer be more explicit about
+     # the parameters that they want to allow the user to submit
+     params.require(:question).permit([:title, :body])
+  end
 
 end
